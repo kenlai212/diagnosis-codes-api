82876045f6cdd6e8d5ed167d11e2be495a29e8a7
Ken Lai <ken.lai212@gmail.com>
2024-05-17 23:25:46 +0800

    short list

--- app.py
+++ app.py
@@ -8,3 +8,4 @@ with open('data/icd_10_short.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
     for row in spamreader:
         service.postDiagnosisCode(row[0],row[1])
+        print(row[0])
